95f5540352bdf004805fab325284381c2f6bebaf
Hezekiah_Matthew_Hosea <matthewhosea06@gmail.com>
2022-11-25 14:02:36 +0700

    Update db

--- motorentfai.sql
+++ motorentfai.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Nov 2022 pada 17.17
--- Versi server: 10.4.25-MariaDB
--- Versi PHP: 8.1.10
+-- Generation Time: Nov 25, 2022 at 08:01 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,7 +26,7 @@ USE `motorentfai`;
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `asuransi`
+-- Table structure for table `asuransi`
 --
 
 DROP TABLE IF EXISTS `asuransi`;
@@ -39,7 +39,7 @@ CREATE TABLE `asuransi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 DROP TABLE IF EXISTS `barang`;
@@ -61,17 +61,23 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`ID_Barang`, `No_Rangka`, `No_Mesin`, `Plat`, `No_BPKB`, `No_STNK`, `Harga_sewa`, `Isi_Silinder`, `Nama_Motor`, `Warna_Motor`, `Tahun_Pembuatan`, `gambar`, `Status`, `FK_ID_ASURANSI`) VALUES
-(1, 'b234212s', 'sd3425f3', 'l54f', 'f324rs3', 'fgert4t', 10000, 125, 'sekupi', 'hihitam', '2019-09-11', NULL, 1, NULL),
-(3, 'kujyhtgre343t5ytg', 'thgtfu76y', 'd 032 l', '3435467876564', '56546575', 200000, 160, 'vario', 'warna warni', '2021-04-14', '1666600464.', 3, NULL);
+(1, 'b234212s', 'sd3425f3', 'l54f', 'f324rs3', 'fgert4t', 10000, 125, 'sekupi', 'hihitam', '2019-09-11', 'OyoOpi5z.jpg', 1, NULL),
+(3, 'kujyhtgre343t5ytg', 'thgtfu76y', 'd 032 l', '3435467876564', '56546575', 200000, 160, 'vario', 'warna warni', '2021-04-14', '8vgfJ7dV.jpg', 1, NULL),
+(4, 'adawsrey', 'rw4t65yref', 'JA2123', 'THDGSER', 'MUHYERG', 56000, 123, 'UNYU', 'UNGU', '2022-11-15', NULL, 0, NULL),
+(5, 'sadas', 'sa', 'DD 012 l', 'a', '123', 10000, 200, 'apple', 'hitem', '2022-11-29', 'WHQpgyBP.jpg', 1, NULL),
+(6, 'ASDWQE', 'SCDAWQ', 'TESD2', 'XCZSDSAW', 'NYHGFGTDRGT', 10000, 200, 'kucing', 'warna warni', '2022-11-25', 'jxeMGMFK.jpg', 1, NULL),
+(7, 'ydtrsetwrqewerey', 'rw4t65yref', 'JA2123', 'THDGSER', 'MUHYERG', 56000, 123, 'UNYU', 'UNGU', '2022-11-15', NULL, 1, NULL),
+(8, 'YRTFSAER', '3REGR43WEF', 'KO 4322', 'GFHFTRE3', 'IUYT4SD', 23000, 500, 'MASYA', 'MERAH', '2022-11-06', NULL, 1, NULL),
+(9, 'ASDWQE', 'SCDAWQ', 'TESD2', 'XCZSDSAW', 'NYHGFGTDRGT', 10000, 200, 'kucing', 'warna warni', '2022-11-25', 'oV0xUDLd.jpg', 1, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jabatan`
+-- Table structure for table `jabatan`
 --
 
 DROP TABLE IF EXISTS `jabatan`;
@@ -81,7 +87,7 @@ CREATE TABLE `jabatan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `jabatan`
+-- Dumping data for table `jabatan`
 --
 
 INSERT INTO `jabatan` (`ID_Jabatan`, `Nama_Jabatan`) VALUES
@@ -92,7 +98,7 @@ INSERT INTO `jabatan` (`ID_Jabatan`, `Nama_Jabatan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `karyawan`
+-- Table structure for table `karyawan`
 --
 
 DROP TABLE IF EXISTS `karyawan`;
@@ -107,7 +113,7 @@ CREATE TABLE `karyawan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `karyawan`
+-- Dumping data for table `karyawan`
 --
 
 INSERT INTO `karyawan` (`ID_Karyawan`, `Nama_Karyawan`, `NomorTelepon_Karyawan`, `Username_Karyawan`, `Password_Karyawan`, `FK_ID_JABATAN`, `KTP_Karyawan`) VALUES
@@ -118,7 +124,7 @@ INSERT INTO `karyawan` (`ID_Karyawan`, `Nama_Karyawan`, `NomorTelepon_Karyawan`,
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `subscription`
+-- Table structure for table `subscription`
 --
 
 DROP TABLE IF EXISTS `subscription`;
@@ -128,17 +134,24 @@ CREATE TABLE `subscription` (
   `Jumlah_Hari` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `subscription`
+--
+
+INSERT INTO `subscription` (`ID_Subscription`, `Nama_Subscription`, `Jumlah_Hari`) VALUES
+(1, '1', 1);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Table structure for table `transaksi`
 --
 
 DROP TABLE IF EXISTS `transaksi`;
 CREATE TABLE `transaksi` (
   `ID_Trans` int(11) NOT NULL,
   `FK_ID_USER` int(11) NOT NULL,
-  `FK_ID_Karyawan` int(11) NOT NULL,
+  `FK_ID_Karyawan` int(11) DEFAULT NULL,
   `FK_ID_Barang` int(11) NOT NULL,
   `Total` int(11) NOT NULL,
   `Tanggal_Trans` date NOT NULL,
@@ -149,10 +162,18 @@ CREATE TABLE `transaksi` (
   `Status` int(2) NOT NULL COMMENT '-1 = ditolak\r\n0 = pending\r\n1 = diterima'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`ID_Trans`, `FK_ID_USER`, `FK_ID_Karyawan`, `FK_ID_Barang`, `Total`, `Tanggal_Trans`, `Start_Date`, `FK_ID_SUBSCRIPTION`, `End_Date`, `Bukti_Bayar`, `Status`) VALUES
+(1, 3, 1, 5, 150000, '2022-09-29', '2022-09-29', 1, '2022-11-17', NULL, 1),
+(2, 3, 1, 1, 150000, '2022-09-29', '2022-09-29', 1, '2022-10-04', NULL, 1);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
@@ -169,7 +190,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`ID_User`, `fullname`, `NIK`, `Telepon`, `Username`, `Email`, `Password`, `Status`, `KTP_User`) VALUES
@@ -180,39 +201,39 @@ INSERT INTO `user` (`ID_User`, `fullname`, `NIK`, `Telepon`, `Username`, `Email`
 --
 
 --
--- Indeks untuk tabel `asuransi`
+-- Indexes for table `asuransi`
 --
 ALTER TABLE `asuransi`
   ADD PRIMARY KEY (`ID_Asuransi`);
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`ID_Barang`),
   ADD KEY `FK_ID_ASURANSI` (`FK_ID_ASURANSI`);
 
 --
--- Indeks untuk tabel `jabatan`
+-- Indexes for table `jabatan`
 --
 ALTER TABLE `jabatan`
   ADD PRIMARY KEY (`ID_Jabatan`);
 
 --
--- Indeks untuk tabel `karyawan`
+-- Indexes for table `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`ID_Karyawan`),
   ADD KEY `FK_ID_JABATAN` (`FK_ID_JABATAN`);
 
 --
--- Indeks untuk tabel `subscription`
+-- Indexes for table `subscription`
 --
 ALTER TABLE `subscription`
   ADD PRIMARY KEY (`ID_Subscription`);
 
 --
--- Indeks untuk tabel `transaksi`
+-- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`ID_Trans`),
@@ -222,69 +243,69 @@ ALTER TABLE `transaksi`
   ADD KEY `FK_ID_SUBSCRIPTION` (`FK_ID_SUBSCRIPTION`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`ID_User`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `asuransi`
+-- AUTO_INCREMENT for table `asuransi`
 --
 ALTER TABLE `asuransi`
   MODIFY `ID_Asuransi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `barang`
+-- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `ID_Barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_Barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `karyawan`
+-- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
   MODIFY `ID_Karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `subscription`
+-- AUTO_INCREMENT for table `subscription`
 --
 ALTER TABLE `subscription`
-  MODIFY `ID_Subscription` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_Subscription` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `transaksi`
+-- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `ID_Trans` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID_Trans` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `ID_User` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `barang`
+-- Constraints for table `barang`
 --
 ALTER TABLE `barang`
   ADD CONSTRAINT `FK_ID_ASURANSI` FOREIGN KEY (`FK_ID_ASURANSI`) REFERENCES `asuransi` (`ID_Asuransi`);
 
 --
--- Ketidakleluasaan untuk tabel `karyawan`
+-- Constraints for table `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD CONSTRAINT `FK_ID_JABATAN` FOREIGN KEY (`FK_ID_JABATAN`) REFERENCES `jabatan` (`ID_Jabatan`);
 
 --
--- Ketidakleluasaan untuk tabel `transaksi`
+-- Constraints for table `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD CONSTRAINT `FK_ID_Barang` FOREIGN KEY (`FK_ID_Barang`) REFERENCES `barang` (`ID_Barang`),
