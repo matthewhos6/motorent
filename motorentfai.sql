c9dc350d46181a0aba5de111f7db48cb047dd197
ANNOnym4u5 <69281220+ANNOnym4u5@users.noreply.github.com>
2022-12-07 02:58:28 +0700

    resource

--- motorentfai.sql
+++ motorentfai.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `motorentfai`
 --
-CREATE DATABASE IF NOT EXISTS `motorentfai` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `motorentfai`;
+CREATE DATABASE IF NOT EXISTS `k1motorent_motorent` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `k1motorent_motorent`;
 
 -- --------------------------------------------------------
 
